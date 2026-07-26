26c388a8b006f41f1a10745c4d5c3065c2c67f29
YASEN-HAROON <yasenmr4@gmail.com>
2026-07-26 13:54:28 +0300

    Delete api directory

--- api/auth/logout.js
+++
@@ -1,7 +0,0 @@
-import { clearAuthCookie } from '../../lib/auth.js';
-
-export default function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
-  clearAuthCookie(res, req);
-  res.status(200).json({ ok: true });
-}
