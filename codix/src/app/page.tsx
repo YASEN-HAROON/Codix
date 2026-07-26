2a5e0601b4030a34532850cf997bb8edcf6dee18
YASEN-HAROON <yasenmr4@gmail.com>
2026-07-26 13:04:26 +0300

    Delete codix directory

--- codix/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation";
-
-// Root is rewritten to index.html via next.config, but keep a fallback
-export default function Home() {
-  redirect("/index.html");
-}
